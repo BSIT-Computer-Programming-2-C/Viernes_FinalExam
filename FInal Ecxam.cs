e7f21bf77be04b1ae9ef8ceadbf41b77541c0969
Jhake Viernes <jhakeviernes11@gmail.com>
2024-12-09 12:12:26 +0800

    Add files via upload

---
+++ FInal Ecxam.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+public abstract class Animal
+{
+    
+    public abstract void MakeSound();
+}
+
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow meow meooww!");
+    }
+}
+
+
+public class Chicken : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("KOK KOK KOK");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     
+        Animal myCat = new Cat(); 
+        Animal myChicken = new Chicken(); 
+
+       
+        Console.WriteLine("cat makes sound:");
+        myCat.MakeSound();
+
+        Console.WriteLine("chicken makes sound:");
+        myChicken.MakeSound();
+    }
+}
+
---
+++ Final Exam.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public class Person
+{
+
+    private string name;
+    private int age;
+    private string address;
+
+
+    public Person(string name, int age, string address)
+    {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+
+    public int Age
+    {
+        get { return age; }
+        set
+        {
+            if (value >= 0)
+                age = value;
+            else
+                throw new ArgumentException("Age must be positive");
+        }
+    }
+
+
+    public string Address
+    {
+        get { return address; }
+        set { address = value; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person("NASUS", 1000, "23 NEXUS VILLAGE");
+
+
+            Console.WriteLine($"Name: {person.Name}");
+            Console.WriteLine($"Age: {person.Age}");
+            Console.WriteLine($"Address: {person.Address}");
+
+
+            person.Name = "NASUS";
+            person.Age = 1000;
+            person.Address = "23 NEXUS VILLAGE";
+
+
+            person.DisplayInfo();
+        }
+    }
+}
